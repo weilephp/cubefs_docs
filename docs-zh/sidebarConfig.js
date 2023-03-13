c9143c65b75d31261b7335a143ffc9d6686ea53d
wuweile <wuweile@oppo.com>
2023-03-13 09:58:25 +0800

    feat: 调试仓库增加补充markdown文档的菜单栏

---
+++ docs-zh/sidebarConfig.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    text: 'GETTING STARTED',
+    children: [
+      {
+        text: 'community',
+        children: [
+          'community/article.md',
+          'community/overview.md',
+        ]
+      },
+      'design/authnode.md',
+      'design/blobstore.md',
+    ]
+  },
+]
---
+++ docs/sidebarConfig.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    text: 'GETTING STARTED',
+    children: [
+      {
+        text: '社区',
+        children: [
+          'community/article.md',
+          'community/overview.md',
+        ]
+      },
+      'design/authnode.md',
+      'design/blobstore.md',
+    ]
+  },
+]
